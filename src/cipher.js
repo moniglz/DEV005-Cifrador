6bc108879fe8e7a7955835eebc6f2911f86e1c0d
moniglz <monie.gop@gmail.com>
2023-02-26 17:57:32 -0600

    Testeo, cambios en interfaz (texto agregado, cambio colores y fonts). Cambio a README.md

--- src/cipher.js
+++ src/cipher.js
@@ -1,54 +1,59 @@
 const cipher = {
  
   encode: function (offset, msg) {
-  
-    let codedMsg = "";
-    //'p' de position
-    //se crea un loop
+    if(typeof msg !== "string" || typeof offset !== "number" ) {
+      throw new TypeError("Datos no válidos");
+    }
+      
+    let codeMsg = "";
+    /*se crea un loop que itera la función hasta terminar de aplicarlo
+    a todo el mensaje proporcionado por el usuario*/
     for (let p = 0; p < msg.length; p++) {
       const letterCode = msg.charCodeAt(p);
       const letterCodeOffset = letterCode + offset;
       const smallLetter = letterCode >= 97 && letterCode <= 122;
       const capitalLetter = letterCode >= 65 && letterCode <= 90;
-      if (smallLetter) {
+      if(smallLetter) {
         const newLetter = ((letterCodeOffset - 97) % 26) + 97;
         const encodeLetter = String.fromCharCode(newLetter);
-        codedMsg += encodeLetter;
-      }else if (capitalLetter) {
+        codeMsg += encodeLetter;
+      } else if(capitalLetter) {
         const newLetter = ((letterCodeOffset - 65) % 26) + 65;
         const encodeLetter = String.fromCharCode(newLetter);
-        codedMsg += encodeLetter;
+        codeMsg += encodeLetter;
       } else {
-        codedMsg += msg[p];
+        codeMsg += msg[p]
       }
     }
-    return codedMsg;
+    return codeMsg
   },
+
   decode: function (offset, msg) {
-    if (typeof msg !== "string" || typeof offset !== "number") {
-      throw new TypeError("son invalidos");
+    if(typeof msg !== "string" || typeof offset !== "number" ) {
+      throw new TypeError("Datos no válidos");
     }
-    let decodedMsg = "";
-    //'p' de position
-    //se crea un loop
+
+    let decodeMsg = "";
+    
     for (let p = 0; p < msg.length; p++) {
       const letterCode = msg.charCodeAt(p);
       const letterCodeOffset = letterCode - offset;
       const smallLetter = letterCode >= 97 && letterCode <= 122;
       const capitalLetter = letterCode >= 65 && letterCode <= 90;
+      //const specialChar = letterCode >= 32 && letterCode <= 64;
       if (smallLetter) {
         const newLetter = ((letterCodeOffset - 122) % 26) + 122;
         const encodeLetter = String.fromCharCode(newLetter);
-        decodedMsg += encodeLetter;
+        decodeMsg += encodeLetter;
       }else if (capitalLetter) {
-        const newLetter = ((letterCodeOffset + 65) % 26) + 65;
+        const newLetter = ((letterCodeOffset - 90) % 26) + 90;
         const encodeLetter = String.fromCharCode(newLetter);
-        decodedMsg += encodeLetter;
+        decodeMsg += encodeLetter;
       }else {
-        decodedMsg += msg[p];
+        decodeMsg += msg[p]
       }
     }
-    return decodedMsg;
+    return decodeMsg;
   },
 
 };
--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 import cipher from './cipher.js';
-/*Creo variables const para que funciones los botones cifrar y descifrar
-*/
+/*Creo variables const para los botones cifrar y descifrar y a través
+del document.getElementById indicamos qué elementos son los que estamos
+llamando. */
 const btnCifrar = document.getElementById("cifrar");
 const btnDescifrar = document.getElementById("descifrar");
 
+/*Llamamos al botón cifrar previamente definido, a través del método
+addEventListener, indicando que al dar 'clic' se obtendrá el mensaje cifrado
+el cual se obtiene del número elegido en el offset, el mensaje del usuario
+y se muestra en la segunda caja de texto*/
 btnCifrar.addEventListener("click", function(){
   const offset = document.getElementById("offset").value;
   const msgI = document.getElementById("msgI").value;
-  const msgF = cipher.encode(+offset, msgI) ;
+  const msgF = cipher.encode(+offset, msgI);
   document.getElementById("msgF").value = msgF;
+  console.log(+offset,msgI,msgF);
 });
+
+/*Llamamos al botón descifrar a través de addEventListener, indicando que al dar 
+'clic' se obtendrá el mensaje descifrado el cual se obtiene del número elegido en 
+el offset, el mensaje del usuario y se muestra en la segunda caja de texto*/
 btnDescifrar.addEventListener("click", function(){
   const offset = document.getElementById("offset").value;
   const msgI = document.getElementById("msgI").value;
   const msgF = cipher.decode(+offset, msgI);
   document.getElementById("msgF").value = msgF;
-})
+  console.log(+offset,msgI,msgF);
+});
 
 console.log(cipher);
